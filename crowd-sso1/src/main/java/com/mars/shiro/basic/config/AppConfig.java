44df8c225b8a02bccb7d472553720619fbc130d1
Mars Yang <marsyang1@gmail.com>
2015-03-10 20:39:57 +0800

    加入 test appconfig

--- crowd-sso1/src/main/java/com/mars/shiro/basic/config/AppConfig.java
+++ crowd-sso1/src/main/java/com/mars/shiro/basic/config/AppConfig.java
@@ -1,7 +1,41 @@
 package com.mars.shiro.basic.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by mars on 2015/3/10.
  */
+@Configuration
+@ComponentScan("com.cy")
 public class AppConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPackagesToScan("com.cy.tech.request.vo");
+        entityManagerFactoryBean.setJpaDialect(new HibernateJpaDialect());
+        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", MySQLDialect.class.getName());
+        properties.put("hibernate.show_sql", "true");
+        entityManagerFactoryBean.setJpaProperties(properties);
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
+        PropertyPlaceholderConfigurer propertyPlaceholderConfigurer = new PropertyPlaceholderConfigurer();
+        propertyPlaceholderConfigurer.setLocations(
+                new Resource[]{
+                        new ClassPathResource("config.properties"),
+                        new ClassPathResource("common_zh_TW.properties")
+                }
+        );
+        // Allow for other PropertyPlaceholderConfigurer instances.
+        propertyPlaceholderConfigurer.setIgnoreUnresolvablePlaceholders(true);
+        return propertyPlaceholderConfigurer;
+    }
 }
